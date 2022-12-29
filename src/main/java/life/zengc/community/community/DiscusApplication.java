86f71b9a22027e5f8b82e8b2b0deb75dedbff665
zengcheng <1265207567@qq.com>
2022-12-29 10:02:21 +0800

    first commit

---
+++ src/main/java/life/zengc/community/community/DiscusApplication.java
@@ -0,0 +1,13 @@
+package life.zengc.community.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscusApplication.class, args);
+    }
+
+}
